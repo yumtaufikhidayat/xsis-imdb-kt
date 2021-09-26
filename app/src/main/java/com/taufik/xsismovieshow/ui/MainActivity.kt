dbd0130a2d29a6879d3351170ad55a202d91113e
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-26 07:01:01 +0700

    Adding dependencies.

--- app/src/main/java/com/taufik/xsismovieshow/ui/MainActivity.kt
+++ app/src/main/java/com/taufik/xsismovieshow/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.taufik.xsismovieshow
+package com.taufik.xsismovieshow.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taufik.xsismovieshow.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
