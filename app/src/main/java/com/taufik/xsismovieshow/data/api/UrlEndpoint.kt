31f6ab0195087d0095ed09e71423a5f6bb8998d7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-28 11:21:28 +0700

    Updating UrlEndpoint file.

--- app/src/main/java/com/taufik/xsismovieshow/data/api/UrlEndpoint.kt
+++ app/src/main/java/com/taufik/xsismovieshow/data/api/UrlEndpoint.kt
@@ -5,6 +5,4 @@ object UrlEndpoint {
     const val API_KEY = "k_7lb8lynv"
     const val BASE_URL = "https://imdb-api.com/en/API/"
     const val TOP_250_MOVIES = "Top250Movies/"
-    const val STARTING_PAGE_INDEX = 1
-    const val PER_PAGE = 10
 }
